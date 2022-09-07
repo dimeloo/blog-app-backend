50ca6d80131cfda3d80e063402ae379110b4dec0
Mike Ortiz <dimelo@Mikes-MacBook-Air.local>
2022-09-06 22:39:00 -0400

    last commit for now

--- index.js
+++ index.js
@@ -3,23 +3,46 @@ import cors from 'cors'
 import { MongoClient } from 'mongodb'
 import 'dotenv/config'
 
+/**********************************************************************/
+// Database Connection
 const URI = process.env.MONGO_URI
 const client = new MongoClient(URI)
 const database = client.db('blog-app')
 const entries = database.collection('entries')
 
-
 client.connect()
 console.log('Connected to Mongo')
-
+const PORT = process.env.PORT
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.listen(PORT, () => console.log('API running on port', PORT))
 
-app.listen(4040, () => console.log('API running on port 4040'))
-
+/**********************************************************************/
+// Get all Blog entries
 app.get('/', async (req, res) => {
-    const allEntries = await entries.find().toArray()
-    res.send(allEntries)
+  const allEntries = await entries.find().toArray()
+  res.send(allEntries)
+})
+
+/**********************************************************************/
+// Add a Blog entry
+app.post('/', async (req, res) => {
+  await entries.insertOne(req.body)
+  res.json('Blog Entry was added')
+})
+
+/**********************************************************************/
+// Delete a Blog entry
+app.delete('/', async (req, res) => {
+  await entries.findOneAndDelete(req.query)
+  res.json('Blog Entry was deleted')
+})
+
+/**********************************************************************/
+// Update a Blog entry
+app.put('/', async (req, res) => {
+  await entries.findOneAndUpdate(req.query, { $set: req.body })
+  res.json('Blog Entry was updated')
 })
